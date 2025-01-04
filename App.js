d9655645306bc34a5933480e9c5866abb0d923b5
truptidhanraj <trupti.nikam3@gmail.com>
2025-01-04 14:31:11 +0530

    add
    add

---
+++ App.js
@@ -0,0 +1,19 @@
+import Shop from './Components/Shop'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import './App.css'
+
+const App = () => {
+    return (
+    <div>
+<Header/>
+<Shop/>
+<Footer/>
+</div>
+    )
+}
+export default App
+
+
+
+
